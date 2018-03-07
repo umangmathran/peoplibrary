0f15605929efb5423c429d2069418e3ef6f84f9c
Umang Agarwal <umangmathran@users.noreply.github.com>
2018-03-07 14:36:26 +0530

    Added arrows to scroll through the booklist.
    Works only in desktop mode.

--- booklist.php
+++ booklist.php
@@ -12,7 +12,7 @@ if ( $_SESSION['logged_in'] != 1 ) {
 else {
     // Use temporary variables for session variables
     $email = $_SESSION['email'];
-	$results = $mysqli->query("SELECT * FROM owns WHERE email='$email'") or die($mysqli->error());
+	$results = $mysqli->query("SELECT DISTINCT * FROM owns WHERE email='$email'") or die($mysqli->error());
 }
 ?>
 <!DOCTYPE html>
@@ -21,22 +21,64 @@ else {
 		<title>People's Library</title><?php
 		include('includes/header.php'); ?>
 			<!-- Added Books Section -->
-			<div class="w3-modal-content w3-padding" id="addedbook">
-				<h4>Books that you have added:</h4><br />		
+			<div class="w3-modal-content w3-padding">
+				<h4>Books that you have added:</h4>
+				<form class="w3-display-topright" style="margin-right:15px; margin-top:15px;">
+					<input type="search" class="w3-input" style="padding-right:55px" name="search" placeholder="Search In Added Book" />
+					<button type="button" class="w3-button w3-orange w3-display-right" name="searchadded"><i class="fa fa-search fa-fw"></i></button>
+				</form>
+				<br />
 				<!-- Book Photo Grid-->
-				<div class="w3-row-padding w3-padding-4 w3-center" id="booklist"><?php
+				<div class="w3-row-padding w3-padding-4 w3-center w3-display-container" id="booklist"><?php
 				foreach ($results as $key => $book)	{
 					$result = $mysqli->query("SELECT * FROM book WHERE bookid='$book[bookid]'") or die($mysqli->error());
 					$currBook =  $result->fetch_assoc();
 					echo '
-					<div class="w3-quarter">
+					<div class="w3-quarter addedBooks w3-padding">
 						<img src="'.$currBook['img'].'" alt="'.$currBook['title'].'" style="height:165px;" />
 						<p>'.$currBook['title'].'</p>
 					</div>';
-				}			
+				}
+				if($key > 4) {
+					$loop = 1;
+					foreach ($results as $key => $book)	{
+						$result = $mysqli->query("SELECT * FROM book WHERE bookid='$book[bookid]'") or die($mysqli->error());
+						$currBook =  $result->fetch_assoc();
+						echo '
+					<div class="w3-quarter addedBooks w3-padding">
+						<img src="'.$currBook['img'].'" alt="'.$currBook['title'].'" style="height:165px;" />
+						<p>'.$currBook['title'].'</p>
+					</div>';
+						if($loop++ == 3) break;
+					}
+				}
 				?> 
+					<button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+					<button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
 				</div>
 			</div>
+			<script>
+				var slideIndex = 1;
+				showDivs(slideIndex);
+
+				function plusDivs(n) {
+					showDivs(slideIndex += n);
+				}
+
+				function showDivs(n) {
+					var i;
+					var x = document.getElementsByClassName("addedBooks");
+					if (n > x.length-3) {slideIndex = 1}
+					if (n < 1) {slideIndex = x.length-3}
+					for (i = 0; i < x.length; i++) {
+						x[i].style.display = "none";  
+					}
+
+					for(i=0; i < 4; i++){
+						x[(slideIndex+i-1)%x.length].style.display = "block";
+					}
+				}
+			</script>
 			<!-- End Added Books Section -->
 
 
